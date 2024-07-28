00cc8eb37db582ac0f29268fe72890865672dfa4
lfbr0 <20361894+lfbr0@users.noreply.github.com>
2024-07-28 14:12:15 +0100

    add gzip encoding

--- src/main/java/http/encoding/GenericEncoder.java
+++ src/main/java/http/encoding/GenericEncoder.java
@@ -1,5 +1,7 @@
 package http.encoding;
 
+import java.io.IOException;
+
 public interface GenericEncoder {
     StringBuffer encode(StringBuffer body);
 }
--- src/main/java/http/encoding/GzipEncoder.java
+++ src/main/java/http/encoding/GzipEncoder.java
@@ -3,13 +3,52 @@ package http.encoding;
 import logger.ApplicationLogger;
 import logger.ApplicationLoggerFactory;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.GZIPOutputStream;
+
 public class GzipEncoder implements GenericEncoder {
 
     private static final ApplicationLogger logger = ApplicationLoggerFactory.getLogger(GzipEncoder.class);
 
     @Override
     public StringBuffer encode(StringBuffer body) {
-        return body;
+        String bodyText = body.toString();
+        logger.info("Proceeding to encode " + bodyText + " as GZIP");
+        byte[] dataToCompress = bodyText.getBytes(StandardCharsets.UTF_8);
+
+        //Convert to Gzip output stream
+        ByteArrayOutputStream byteStream = null;
+        GZIPOutputStream zipStream = null;
+        try {
+            byteStream = new ByteArrayOutputStream(dataToCompress.length);
+            zipStream = new GZIPOutputStream(byteStream);
+            zipStream.write(dataToCompress);
+        }
+        catch (IOException ex) {
+            logger.error("Failed to convert to gzip output stream", ex);
+            return body;
+        }
+
+        //Close streams
+        try {
+            zipStream.close();
+            byteStream.close();
+        }
+        catch (IOException ex) {
+            logger.error("Failed to close encoding streams", ex);
+            return body;
+        }
+
+        //Get converted stream bytes and return body
+        byte[] compressedData = byteStream.toByteArray();
+        StringBuffer convertedBody = new StringBuffer(compressedData.length);
+        for (byte compressedByte : compressedData) {
+            convertedBody.append((char) compressedByte);
+        }
+
+        return convertedBody;
     }
 
 }
